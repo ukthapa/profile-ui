b8cc4c71d1f8e1fa28788c660445000d9cee9eb4
ukthapa <ukthapa@gmail.com>
2015-08-10 19:05:09 +0530

    includes and template file created

---
+++ template.php
@@ -0,0 +1,66 @@
+<?php include 'inc/site-url.inc.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'inc/before-meta.inc.php'; ?>
+	<meta charset="UTF-8">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Profile Template</title>
+
+	<?php include 'inc/after-meta.inc.php'; ?>
+
+	<!-- Bootstrap CSS-->
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+	<!-- Theme Style-->
+	<link rel="stylesheet" href="theme/theme.css">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+   <?php include 'inc/before-body-start.inc.php'; ?>
+
+</head>
+<body>
+
+ 	<?php include 'inc/after-body-start.inc.php'; ?>
+
+	<header>
+		<?php include 'inc/header.inc.php'; ?>
+	 	<?php include 'inc/nav.inc.php'; ?>
+	</header>
+
+	<div class="content">
+		
+	</div>
+
+
+	<footer>
+		<?php include 'inc/footer.inc.php'; ?>
+	</footer>
+
+	
+
+
+	<!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+	<!-- Bootstrap Script-->
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+	<!-- Custom JS -->
+	<script type="text/javascript" src="js/custom.js"></script>
+
+
+	<?php include 'inc/before-body-end.inc.php'; ?>
+	
+</body>
+</html>
