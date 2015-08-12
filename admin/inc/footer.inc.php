d58246edc78c10dde7536756b48da74349593a22
ukthapa <ukthapa@gmail.com>
2015-08-12 19:14:45 +0530

    php coding started

---
+++ admin/inc/footer.inc.php
@@ -0,0 +1,17 @@
+<div class="contianer">
+	<div class="row">
+		<div class="col-12-lg mt30">
+			<p class="text-center">&copy; Copyright
+				<script type="text/javascript">
+				var d = new Date();
+				document.write(d.getFullYear());
+				</script>,
+			Profile UI
+			</p>
+				<p class="text-center small">
+				You are free to you this project for your personal use.
+			</p>
+		</div>
+	</div>
+	
+</div>
